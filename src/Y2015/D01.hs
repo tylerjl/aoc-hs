5df82cd6bddb03865898c0975c240174ff5c3ebe
Tyler Langlois <tjl@byu.net>
2015-12-03 00:00:37 -0500

    Solutions for day 1 and day 2, no tests yet.

---
+++ src/Y2015/D01.hs
@@ -0,0 +1,27 @@
+#!/usr/bin/env runhaskell
+
+module Y2015.D01 where
+
+import Data.List (foldl')
+
+move :: Char -> Int
+move c | c == '('  = 1
+       | otherwise = -1
+
+level :: String -> Int
+level = foldl' (+) 0 . map move
+
+basement :: String -> Maybe Int
+basement = find 0 1 . map move
+    where find current idx (move:moves)
+               | current + move < 0 = Just idx
+               | otherwise          = find (current + move) (idx + 1) moves
+          find _ _ [] = Nothing
+
+main :: IO ()
+main = do
+       input <- readFile "src/Y2015/D01_input"
+       putStr "Part A: instructions arrive at floor "
+       print (level input)
+       putStr "Part B: basement arrival at position "
+       print (basement input)
---
+++ src/Y2015/D02.hs
@@ -0,0 +1,61 @@
+#!/usr/bin/env runhaskell
+
+import Data.List (foldl', sort)
+import qualified Text.Parsec as P
+import Text.Parsec.Char (char, digit, endOfLine)
+import Text.Parsec.String (Parser)
+
+data Present = Present Int Int Int deriving (Show)
+
+-- |Generic parsing wrapper
+regularParse :: Parser a -> String -> Either P.ParseError a
+regularParse p = P.parse p ""
+
+presentsParser :: Parser [Present]
+presentsParser = P.many1 (presentParser <* P.optional endOfLine)
+
+presentParser :: Parser Present
+presentParser = Present <$> (dimension <* char 'x')
+                        <*> (dimension <* char 'x')
+                        <*>  dimension
+
+dimension :: Parser Int
+dimension = read <$> P.many1 digit
+
+parsePresents :: String -> Maybe [Present]
+parsePresents s = case regularParse presentsParser s of
+                       Right ps -> Just ps
+                       Left _   -> Nothing
+
+surfaceArea :: [Present] -> Int
+surfaceArea = foldl' (+) 0 . map wrapping
+
+wrapping :: Present -> Int
+wrapping p@(Present l w h) = sqft p + (product $ smallest p)
+
+smallest :: Present -> [Int]
+smallest (Present l w h) = take 2 $ sort [l, w, h]
+
+sqft :: Present -> Int
+sqft (Present l w h) = 2*l*w + 2*w*h + 2*h*l
+
+area :: Present -> Int
+area (Present l w h) = l*w*h
+
+ribbonLength :: [Present] -> Int
+ribbonLength = sum . map presentRibbon
+
+presentRibbon :: Present -> Int
+presentRibbon p = (sum $ map (*2) $ smallest p) + area p
+
+main :: IO ()
+main = do
+       input <- readFile "src/Y2015/D02_input"
+       case regularParse presentsParser input of
+            Right presents -> do
+               putStr "Part A: total square feet needed: "
+               print (surfaceArea presents)
+               putStr "Part B: total feet of ribbon needed: "
+               print (ribbonLength presents)
+            Left _         -> do
+                putStrLn "Error: Malformed input."
