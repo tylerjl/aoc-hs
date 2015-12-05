b766030da192a2d0d092ef4265f628ceeabed1a2
Tyler Langlois <tjl@byu.net>
2015-12-04 21:06:12 -0500

    Day 4 solutions

---
+++ src/Y2015/D04.hs
@@ -0,0 +1,34 @@
+#!/usr/bin/env runhaskell
+
+module Y2015.D04 (mine) where
+
+import           Codec.Binary.UTF8.String (encode, decode)
+import           Data.Digest.Pure.MD5 (md5)
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy as B
+import           Data.ByteString.Lazy (ByteString)
+import           Data.Word (Word8)
+
+prefix :: Int -> ByteString
+prefix n = B.replicate (fromIntegral n) $ head $ encode "0"
+
+hash :: Int -> [Word8] -> ByteString
+hash i s = B.pack $ encode $ show $ md5 $ B.pack $ guess i s
+
+guess :: Int -> [Word8] -> [Word8]
+guess = flip (++) . encode . show
+
+solve :: ByteString -> [Word8] -> Int
+solve solution key = find 0
+    where find i | solution `B.isPrefixOf` (hash i key) = i
+                 | otherwise = find (i+1)
+
+mine :: String -> Int -> Int
+mine s n = solve (prefix n) (encode s)
+
+main :: IO ()
+main = do
+       putStr "Part A - key is: "
+       print (mine "iwrupvqb" 5)
+       putStr "Part B - key is: "
+       print (mine "iwrupvqb" 6)
---
+++ test/Y2015/D04Spec.hs
@@ -0,0 +1,14 @@
+module Y2015.D04Spec (spec) where
+
+import Y2015.D04
+
+import Test.Hspec
+
+spec :: Spec
+spec =
+    describe "Day 4" $ do
+        describe "mine" $ do
+            it "should solve abcdef" $
+                mine "abcdef" 5 `shouldBe` 609043
+            it "should solve pqrstuv" $
+                mine "pqrstuv" 5 `shouldBe` 1048970
