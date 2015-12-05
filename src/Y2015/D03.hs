985b343e5dc029cd0fc4e41c594e36a9f5ed3b99
Tyler Langlois <tjl@byu.net>
2015-12-04 20:16:18 -0500

    Use a set for day 3 instead of a map

--- src/Y2015/D03.hs
+++ src/Y2015/D03.hs
@@ -2,10 +2,9 @@
 
 module Y2015.D03 (solve, roboSolve) where
 
-import           Data.Bifunctor
-import           Data.List (foldl')
-import           Data.Map (Map)
-import qualified Data.Map as Map
+import           Data.List   (foldl')
+import           Data.Set    (Set)
+import qualified Data.Set as Set
 
 type Point = (Int, Int)
 
@@ -16,15 +15,18 @@ direction c | c == '^'  = (0,  1)
             | c == '<'  = (-1, 0)
             | otherwise = (0,  0)
 
+start :: Set Point
+start = Set.singleton (0, 0)
+
 move :: Point -> Point -> Point
 move (dx, dy) (x, y) = (x + dx, y + dy)
 
 solve :: String -> Int
-solve = Map.size . deliver . map direction
+solve = Set.size . deliver start . map direction
 
 roboSolve :: String -> Int
-roboSolve = Map.size . teamDelivery . tMap direction . divideWork
-    where teamDelivery (santa, robot) = navigate (0, 0) (deliver santa) robot
+roboSolve = Set.size . teamDelivery . tMap direction . divideWork
+    where teamDelivery = uncurry (deliver . deliver start)
 
 tMap :: (a -> b) -> ([a], [a]) -> ([b], [b])
 tMap f (a1, a2) = (map f a1, map f a2)
@@ -35,17 +37,15 @@ divideWork [x] = ([x], [])
 divideWork (x:y:zs) = (x:xp, y:yp)
                       where (xp, yp) = divideWork zs
 
-deliver :: [Point] -> Map Point Int
-deliver = navigate (0, 0) (Map.singleton (0, 0) 1)
+deliver :: Set Point -> [Point] -> Set Point
+deliver = navigate (0, 0)
 
-navigate :: Point -> Map Point Int -> [Point] -> Map Point Int
+navigate :: Point -> Set Point -> [Point] -> Set Point
 navigate origin history []          = history
 navigate origin history (dir:plans) =
         let newPoint = move dir origin
-        in
-            navigate newPoint (Map.insertWith record newPoint 1 history) plans
-        where
-            record new old = new + old
+            step     = Set.insert newPoint history
+        in  navigate newPoint step plans
 
 main :: IO ()
 main = do
