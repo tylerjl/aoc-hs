222d8af3d64da1349b47c7eba0793198f03b54b9
Tyler Langlois <tjl@byu.net>
2015-12-22 20:16:07 -0500

    Really bad attempt at day 19 part 2

--- src/Y2015/D19.hs
+++ src/Y2015/D19.hs
@@ -1,15 +1,39 @@
 module Y2015.D19 (distinctMols) where
 
+import Control.Arrow ((***))
 import Data.Char (isLower, isUpper)
-import Data.List (foldl', inits, tails)
-import Data.Map.Strict (Map)
+import Data.Function (on)
+import Data.List (foldl', inits, isInfixOf, sortBy, tails)
+import Data.List.Utils (replace)
+import Data.Map.Strict (Map, fromList, keys)
 import qualified Data.Map.Strict as M
 import Data.Set  (Set)
 import qualified Data.Set as S
+import Data.Tuple (swap)
 
 type Mol       = [String]
 type Repls     = Map String (Set String)
 type Compounds = Set Mol
+
+molSteps :: String -> Int
+molSteps s = subSteps 0 repls subs mol
+  where input = map reverse $ lines s
+        mol   = last input
+        repls = fromList . map (molPair id . words) $ init input
+        subs = sortBy (compare `on` length) $ keys repls
+
+subSteps :: Int -> Map String String -> [String] -> String -> Int
+subSteps _ _     []            _ = 5
+subSteps n repls subs@(sub:ss) m
+  | n > 10000         = 0
+  | m == "e"          = n
+  | sub `isInfixOf` m = subSteps (n+1) repls ss subbed
+  | otherwise         = subSteps n     repls ss m
+  where subbed      = replace sub replacement m
+        replacement = repls M.! sub
+
+-- subSteps :: String -> [(String, String)] -> Int
+-- subSteps mol ((a,b):xs) |
 
 distinctMols :: String -> Int
 distinctMols s = S.size $ compounds mols repls
