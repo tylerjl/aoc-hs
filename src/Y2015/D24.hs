a52d9d1a889cc1d9cb7b8ce3395974630fb059f8
Tyler Langlois <tjl@byu.net>
2016-09-10 18:16:05 -0400

    Faster D24, but still slow

--- src/Y2015/D24.hs
+++ src/Y2015/D24.hs
@@ -1,8 +1,8 @@
-module Y2015.D24 (idealEntanglement) where
+module Y2015.D24  (idealEntanglement) where
 
-import Data.List (minimumBy, permutations)
+import Data.List  (minimumBy, permutations)
 import Data.Maybe (mapMaybe)
-import Data.Ord (comparing)
+import Data.Ord   (comparing)
 
 idealEntanglement :: Int    -- ^ How many compartments to divide amongst.
                   -> String -- ^ Input as a string containing newlined-separated
@@ -10,31 +10,19 @@ idealEntanglement :: Int    -- ^ How many compartments to divide amongst.
                   -> Int    -- ^ Ideal entanglement value for the resulting
                             --   package distribution.
 idealEntanglement compartments input =
-    case minGroups balanced weights of
-        [] -> -1
-        l  -> minimum $ map product $ filter ((==) balanced . sum) l
+    product $ head $ filter isBalanced $ head $
+        filter (any isBalanced) $
+        allSplits $ permutations weights
     where weights  = map read $ lines input
           balanced = sum weights `quot` compartments
+          isBalanced = (==) balanced . sum
 
-minGroups :: Int -> [Int] -> [[Int]]
-minGroups balance weights =
-    minimumBy (comparing groupCardinality) $
-        mapMaybe (go 1 balance) orderings
-    where
-        orderings = permutations weights
-        go n w l | n >= length l = Nothing
-                 | otherwise     =
-                     if not $ null balanced
-                         then Just balanced
-                         else go (n+1) w l
-                     where
-                         compartments = splitEvery n l
-                         balanced = filter ((==) w . sum)
-                             compartments
-
-groupCardinality :: [[a]] -> Int
-groupCardinality = minimum . map length
+allSplits :: [[a]] -> [[[a]]]
+allSplits = flip concatMap [1..] . go
+    where go ps n = map (splitEvery n) ps
 
 -- Credit to: https://stackoverflow.com/a/8700618
 splitEvery :: Int -> [a] -> [[a]]
-splitEvery n = takeWhile (not . null) . map (take n) . iterate (drop n)
+splitEvery n = takeWhile (not . null)
+             . map (take n)
+             . iterate (drop n)
