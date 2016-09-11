15226f1bd2186ea11a58818a75e7321dd91707fb
Tyler Langlois <tjl@byu.net>
2016-09-10 20:54:44 -0400

    Prettified day 24 solution

--- src/Y2015/D24.hs
+++ src/Y2015/D24.hs
@@ -1,20 +1,23 @@
 module Y2015.D24 (idealEntanglement) where
 
-import Data.List (sortBy, subsequences)
+import Data.List   (minimumBy, subsequences)
+import Data.Monoid (mconcat)
+import Data.Ord    (comparing)
 
+-- |Find the ideal entanglement value for a given input of packages.
 idealEntanglement :: Int    -- ^ How many compartments to divide amongst.
-                  -> String -- ^ Input as a string containing newlined-separated
-                            --   list of package weights.
-                  -> Int    -- ^ Ideal entanglement value for the resulting
-                            --   package distribution.
+                  -> String {-^
+                                Input as a string containing newlined-separated
+                                list of package weights.
+                            -}
+                  -> Int    {-^
+                                Ideal entanglement value for the resulting
+                                package distribution.
+                            -}
 idealEntanglement compartments input =
-    product $ head seqs
+    product $ minimumBy (mconcat santasPredicates) seqs
     where weights  = map read $ lines input
           balanced = sum weights `quot` compartments
           isBalanced = (==) balanced . sum
-          seqs = sortBy idealPackage $
-              filter isBalanced $
-              subsequences weights
-
-idealPackage a b | length a == length b = product a `compare` product b
-                 | otherwise = length a `compare` length b
+          seqs = filter isBalanced $ subsequences weights
+          santasPredicates = [comparing length, comparing product]
