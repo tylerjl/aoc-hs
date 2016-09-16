47a36c0f1e6d205c11f46adcecb1906d86c2a672
Tyler Langlois <tjl@byu.net>
2016-09-15 21:40:33 -0400

    Add documentation coverage testing

---
+++ test/Haddock.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import qualified Control.Foldl as Fold
+import           Data.List        (genericLength)
+import           Data.Maybe       (catMaybes)
+import           Data.Text        (unpack)
+import           System.Exit      (exitFailure, exitSuccess)
+import           Text.Regex       (matchRegex, mkRegex)
+import           Turtle           (inprocWithErr, fold)
+
+average :: (Fractional a, Real b) => [b] -> a
+average xs = realToFrac (sum xs) / genericLength xs
+
+expected :: Fractional a => a
+expected = 90
+
+main :: IO ()
+main = do
+    text <- fold
+        (fmap (\(Left o) -> o) (inprocWithErr "stack" ["haddock"] ""))
+        Fold.list
+    let output = map unpack text
+        avg = average $ match output
+    if avg >= expected
+        then exitSuccess
+        else exitFailure
+
+match :: [String] -> [Int]
+match = fmap read . concat . catMaybes . fmap (matchRegex p)
+  where p = mkRegex "^ *([0-9]*)% "
