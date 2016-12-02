a7c4ef58f3494c7b870ea01e1d87e7d0312f6a3f
Tyler Langlois <tjl@byu.net>
2016-12-02 11:51:27 -0500

    Benchmark 2016 day 2

--- benchmark/Y2016/Bench.hs
+++ benchmark/Y2016/Bench.hs
@@ -7,6 +7,13 @@ import Criterion.Main (env)
 
 import Y2016
 
+bathroomDirections = unlines
+    [ "ULL"
+    , "RRDDD"
+    , "LURDL"
+    , "UUUUD"
+    ]
+
 benchmarks :: Benchmark
 benchmarks =
     bgroup "Y2016"
@@ -17,6 +24,12 @@ benchmarks =
               ]
             , bgroup "visitedTwice"
               [ bench "simple" $ nf visitedTwice "R8, R4, R4, R8"
+              ]
+            ]
+        , bgroup "Day 2"
+            [ bgroup "bathroomCode"
+              [ bench "part 1" $ nf (bathroomCode grid1 (2,2)) bathroomDirections
+              , bench "part 2" $ nf (bathroomCode grid2 (1,3)) bathroomDirections
               ]
             ]
         ]
