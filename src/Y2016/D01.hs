5f3379570ecde22e32b08155e2a4fc937ca30abb
Tyler Langlois <tjl@byu.net>
2016-12-02 11:42:42 -0500

    hindent-format 2016 day 1 and 2

--- src/Y2016/D01.hs
+++ src/Y2016/D01.hs
@@ -15,70 +15,78 @@ import Data.List (foldl')
 import qualified Data.Set as Set
 
 -- | Cardinal direction for movement
-data Direction = North
-               | East
-               | South
-               | West
-               deriving (Enum)
+data Direction
+  = North
+  | East
+  | South
+  | West
+  deriving (Enum)
 
 -- | (x, y) coordinates
 type Coordinates = (Int, Int)
 
 -- | Find number of blocks traveled per list of instructions
-blockDistance :: String -- ^ Input of directions
-              -> Int -- ^ Number of blocks traveled
-blockDistance = blocks
-              . foldl' travel (0, 0)
-              . mapRoute
+blockDistance
+  :: String -- ^ Input of directions
+  -> Int -- ^ Number of blocks traveled
+blockDistance = blocks . foldl' travel (0, 0) . mapRoute
 
 -- | Find first block that is visited twice
-visitedTwice :: String -- ^ Input of directions
-             -> Maybe Int -- ^ Possible block that is visited twice
-visitedTwice = navigate (0,0) Set.empty
-             . mapRoute
-    where navigate point set (path:xs)
-              | Set.member point' set = Just $ blocks point'
-              | otherwise = navigate point' (Set.insert point' set) xs
-              where point' = travel point path
-          navigate _ _ _ = Nothing
+visitedTwice
+  :: String -- ^ Input of directions
+  -> Maybe Int -- ^ Possible block that is visited twice
+visitedTwice = navigate (0, 0) Set.empty . mapRoute
+  where
+    navigate point set (path:xs)
+      | Set.member point' set = Just $ blocks point'
+      | otherwise = navigate point' (Set.insert point' set) xs
+      where
+        point' = travel point path
+    navigate _ _ _ = Nothing
 
 -- | Translate an input string into a list of movement `Direction`s
-mapRoute :: String -- ^ Input string
-         -> [Direction] -- ^ List of `Direction`s
-mapRoute = toRoute North
-         . map (toPath . filter  (not . (==) ','))
-         . words
-         where toRoute orientation ((lr, distance):xs) =
-                   replicate distance orientation' ++ toRoute orientation' xs
-                   where orientation' = turn lr orientation
-               toRoute _ [] = []
+mapRoute
+  :: String -- ^ Input string
+  -> [Direction] -- ^ List of `Direction`s
+mapRoute = toRoute North . map (toPath . filter (not . (==) ',')) . words
+  where
+    toRoute orientation ((lr, distance):xs) =
+      replicate distance orientation' ++ toRoute orientation' xs
+      where
+        orientation' = turn lr orientation
+    toRoute _ [] = []
 
 -- | Calculate the number of blocks from the origin
-blocks :: Coordinates -- ^ Current `Coordinates`
-       -> Int -- ^ Number of blocks that `Coordinate` lies from the origin
+blocks
+  :: Coordinates -- ^ Current `Coordinates`
+  -> Int -- ^ Number of blocks that `Coordinate` lies from the origin
 blocks (x, y) = abs x + abs y
 
 -- | Move along a given direction
-travel :: Coordinates -- ^ Initial `Coordinates`
-       -> Direction -- ^ `Direction` to move towards
-       -> Coordinates -- ^ New `Coordinates`
-travel (x, y) d = case d of
-                    North -> (x, succ y)
-                    East -> (succ x, y)
-                    South -> (x, pred y)
-                    West -> (pred x, y)
+travel
+  :: Coordinates -- ^ Initial `Coordinates`
+  -> Direction -- ^ `Direction` to move towards
+  -> Coordinates -- ^ New `Coordinates`
+travel (x, y) d =
+  case d of
+    North -> (x, succ y)
+    East -> (succ x, y)
+    South -> (x, pred y)
+    West -> (pred x, y)
 
 -- | Translate a movement instruction to an intermediate form
-toPath :: String -- ^ Raw input instruction
-       -> (Char, Int) -- ^ `Tuple` consisting of movement direction and
-                      -- ^ distance
+toPath
+  :: String -- ^ Raw input instruction
+  -> (Char, Int) -- ^ `Tuple` consisting of movement direction and
+                 -- ^ distance
 toPath (direction:steps) = (direction, read steps)
 toPath [] = (' ', 0)
 
 -- | Determine new direction after turning Left or Right
-turn :: Char -- ^ 'L'eft or 'R'right
-     -> Direction -- ^ Initial `Direction`
-     -> Direction -- ^ New `Direction`
+turn
+  :: Char -- ^ 'L'eft or 'R'right
+  -> Direction -- ^ Initial `Direction`
+  -> Direction -- ^ New `Direction`
 turn 'R' West = North
 turn 'L' North = West
 turn 'R' d = succ d
--- src/Y2016/D02.hs
+++ src/Y2016/D02.hs
@@ -18,64 +18,72 @@ import qualified Data.Matrix as Matrix
 type Position = (Int, Int)
 
 -- | Wrapper to construct a `Matrix`
-grid :: Int -- ^ Rows
-     -> Int -- ^ Columns
-     -> [a] -- ^ Elements
-     -> Matrix.Matrix a -- ^ Resulting `Matrix`
+grid
+  :: Int -- ^ Rows
+  -> Int -- ^ Columns
+  -> [a] -- ^ Elements
+  -> Matrix.Matrix a -- ^ Resulting `Matrix`
 grid = Matrix.fromList
 
 -- | `Matrix` for part 1
 grid1 :: Matrix.Matrix String
-grid1 = grid 3 3 (map show ([1..] :: [Int]))
+grid1 = grid 3 3 (map show ([1 ..] :: [Int]))
 
 -- | `Matrix` for part 2
 grid2 :: Matrix.Matrix String
-grid2 = grid 5 5 $
-  [ "", "",  "1",  "", ""
+grid2 =
+  grid 5 5 $
+  [ "", "",  "1", "",  ""
   , "", "2", "3", "4", ""
-  ] ++ map show ([5..9] :: [Int]) ++
+  ] ++ map show ([5 .. 9] :: [Int]) ++
   [ "", "A", "B", "C", ""
-  , "", "",  "D", "",  ""
+  , "",  "", "D", "",  ""
   ]
 
-bathroomCode :: Matrix.Matrix String -- ^ Grid to solve for
-             -> Position -- ^ Starting `Position` (y, x)
-             -> String -- ^ Input `String` of movement instructions
-             -> String -- ^ Bathroom code
-bathroomCode m origin = decode m "" origin
-                      . lines
+bathroomCode
+  :: Matrix.Matrix String -- ^ Grid to solve for
+  -> Position -- ^ Starting `Position` (y, x)
+  -> String -- ^ Input `String` of movement instructions
+  -> String -- ^ Bathroom code
+bathroomCode m origin = decode m "" origin . lines
 
-decode :: Matrix.Matrix String -- ^ Grid to solve for
-       -> String -- ^ Solution key that's being built up
-       -> Position -- ^ Current position
-       -> [String] -- ^ List of movements for each code character
-       -> String -- ^ Solution
+decode
+  :: Matrix.Matrix String -- ^ Grid to solve for
+  -> String -- ^ Solution key that's being built up
+  -> Position -- ^ Current position
+  -> [String] -- ^ List of movements for each code character
+  -> String -- ^ Solution
 decode _ key _ [] = key
 decode m key position (moves:xs) =
   decode m (key ++ Matrix.getElem y x m) position' xs
-  where position'@(x, y) = translate m position moves
+  where
+    position'@(x, y) = translate m position moves
 
-translate :: Matrix.Matrix [a] -- ^ `Matrix` to move within bounds of
-          -> Position -- ^ Starting `Position`
-          -> String -- ^ List of directions to move
-          -> Position -- ^ Final position after performing movements
+translate
+  :: Matrix.Matrix [a] -- ^ `Matrix` to move within bounds of
+  -> Position -- ^ Starting `Position`
+  -> String -- ^ List of directions to move
+  -> Position -- ^ Final position after performing movements
 translate _ position [] = position
 translate m position (x:xs)
   | withinBounds position' m = translate m position' xs
   | otherwise = translate m position xs
-  where position' = move position x
+  where
+    position' = move position x
 
-withinBounds :: Position -- ^ Coordinates of position to check
-             -> Matrix.Matrix [a] -- ^ `Matrix` to test against
-             -> Bool -- ^ Whether the given position lies within the matrix
+withinBounds
+  :: Position -- ^ Coordinates of position to check
+  -> Matrix.Matrix [a] -- ^ `Matrix` to test against
+  -> Bool -- ^ Whether the given position lies within the matrix
 withinBounds (x, y) m =
   case Matrix.safeGet y x m of
     Nothing -> False
     Just v -> not $ null v
 
-move :: Position -- ^ Initial `Position`
-     -> Char -- ^ Movement directive (any of "UDLR")
-     -> Position -- ^ New `Position`
+move
+  :: Position -- ^ Initial `Position`
+  -> Char -- ^ Movement directive (any of "UDLR")
+  -> Position -- ^ New `Position`
 move (x, y) c =
   case c of
     'U' -> (x, y - 1)
