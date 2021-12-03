17362e533ab5e898e3585e5ae4ffbeffcad64eff
Tyler Langlois <tjl@byu.net>
2021-12-03 01:25:23 -0700

    2021 day 3 part b

--- src/Y2021/D03.hs
+++ src/Y2021/D03.hs
@@ -3,19 +3,25 @@ module Y2021.D03 where
 import Data.Text (Text)
 import qualified Data.Text as T
 import GHC.Exts
-import Data.List (transpose, group, sort)
-import Data.List.Extra (maximumOn, minimumOn)
+import Data.List (transpose, group, sort, sortOn, groupBy)
+import Data.Function (on)
 
 type Bit  = Int
 type Bits = [Bit]
 
 part3A :: Text -> Float
 part3A (toBits -> input)
-  = bitsToDec (commonOn maximumOn input)
-  * bitsToDec (commonOn minimumOn input)
+  = bitsToDec (commonOn id input)
+  * bitsToDec (commonOn Down input)
 
-part3B :: Text -> Int
-part3B = error "not implemented"
+part3B :: Text -> Float
+part3B (toBits -> input) = oxygen * co2
+  where oxygen = pinpoint 0 input id
+        co2 = pinpoint 0 input Down
+        pinpoint _ [] _ = error "couldn't find matching bits"
+        pinpoint _ [x] _ = bitsToDec x
+        pinpoint idx bits f = let common = commonOn f bits
+          in pinpoint (succ idx) (keepAt bits (common !! idx) idx) f
 
 keepAt :: [Bits] -> Bit -> Int -> [Bits]
 keepAt bitsList bit idx = filter ((==) bit . flip (!!) idx) bitsList
@@ -27,8 +33,12 @@ bitsToDec = go 0 . reverse
           | otherwise = go (succ n) xs
         go _ []     = 0
 
-commonOn :: ((Bits -> Int) -> [Bits] -> Bits) -> [Bits] -> Bits
-commonOn f = map (head . f length . group . sort) . transpose
+commonOn :: Ord b => (Int -> b) -> [[Int]] -> [Int]
+commonOn f =
+  map ( head . head . head . map (sortOn (f . head))
+        . groupBy ((==) `on` length) . sortOn (f . length) . group . sort)
+  -- Turn rows into lists of each position
+  . transpose
 
 toBits :: Text -> [Bits]
 toBits = map (map toBit . toList) . T.lines
